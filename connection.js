9ab43da459adbe2ea08d13e290b0104accf555ef
KyojuHashira <139628002+KyojuHashira@users.noreply.github.com>
2023-08-11 13:14:21 -0500

    Fixed some issues

--- connection.js
+++ connection.js
@@ -1,3 +1,13 @@
+const phrases = {
+    0: "Stars align in favor.",
+    1: "The cosmos whispers 'yes'.",
+    2: "Fate nods affirmatively.",
+    3: "Eclipses cast doubt.",
+    4: "Winds blow the other way.",
+    5: "The horizon stays dim.",
+    6: "Mysteries remain veiled."
+};
+
 const contractAddress = "0xDe9F6ceDda947d8484807b68475DDA5A18Fb89BF";
 const contractABI = [
     {
@@ -48,12 +58,79 @@ const contractABI = [
     }
 ];
 
+function traverseList(startIndex, steps) {
+    const listLength = Object.keys(phrases).length;
+    for (let i = 0; i < steps; i++) {
+        startIndex = (startIndex + 1) % listLength;
+    }
+    return phrases[startIndex];
+}
+
 async function connectToWeb3() {
     if (window.ethereum) {
         window.web3 = new Web3(window.ethereum);
         await window.ethereum.enable();
         console.log("Connected to Web3!");
     } else {
-        console.error("Please install Metamask or Web3-enabled browser.");
+        console.error("Web3 not found. Please install Metamask or use a Web3-enabled browser.");
     }
 }
+
+function wipeOutEffect(element, callback) {
+    element.classList.add('fade-out');
+    
+    const transitionDuration = 800;
+    setTimeout(() => {
+        element.classList.remove('fade-out');
+        callback();
+    }, transitionDuration);
+}
+
+function showResult(finalPhrase) {
+    const circularWidget = document.querySelector('.circular-widget');
+    const resultContainer = document.createElement('div');
+    resultContainer.className = 'magic-result';
+    resultContainer.textContent = finalPhrase;
+    resultContainer.setAttribute('data-text', finalPhrase);
+
+    wipeOutEffect(circularWidget, () => {
+        circularWidget.innerHTML = '';
+        const newStarsEffect = document.createElement('div');
+        newStarsEffect.id = 'stars-effect';
+        newStarsEffect.className = 'stars-effect';
+        starsEffect = newStarsEffect;
+        circularWidget.appendChild(newStarsEffect);
+        circularWidget.appendChild(resultContainer);
+    });
+}
+
+async function getRandomNumberFree() {
+    try {
+        const web3ForeTellerContract = new web3.eth.Contract(contractABI, contractAddress);
+        const result = await web3ForeTellerContract.methods.getRandomNumber().call();
+        const randomSteps = Math.floor(Math.random() * 7);
+        const finalPhrase = traverseList(Number(result), randomSteps);
+        showResult(finalPhrase);
+    } catch (error) {
+        console.error("Error calling smart contract method:", error);
+    }
+}
+
+async function getRandomNumberGasBurn() {
+    try {
+        const web3ForeTellerContract = new web3.eth.Contract(contractABI, contractAddress);
+        const accounts = await web3.eth.getAccounts();
+        const fromAddress = accounts[0];
+        await web3ForeTellerContract.methods.getRandomSaltFromGasBurn().send({ from: fromAddress, gas: 200000 });
+        const result = await web3ForeTellerContract.methods.getRandomNumberWithGas().call();
+        const randomSteps = Math.floor(Math.random() * 7);
+        const finalPhrase = traverseList(Number(result), randomSteps);
+        showResult(finalPhrase);
+    } catch (error) {
+        console.error("Error calling smart contract method:", error);
+    }
+}
+
+window.addEventListener("load", () => {
+    connectToWeb3();
+});
