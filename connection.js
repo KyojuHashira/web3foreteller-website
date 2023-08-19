81d764f49b489d73b809dfe0d4b22cf53f727edb
KyojuHashira <139628002+KyojuHashira@users.noreply.github.com>
2023-08-19 00:55:44 -0500

    random salt added + bug fixes

--- connection.js
+++ connection.js
@@ -7,6 +7,7 @@ const phrases = {
     5: "The horizon stays dim.",
     6: "Mysteries remain veiled."
 };
+const magicSaltMessage = "To generate and use magic salt, burn some gas!";
 
 const contractAddress = "0xDe9F6ceDda947d8484807b68475DDA5A18Fb89BF";
 const contractABI = [
@@ -76,56 +77,54 @@ async function connectToWeb3() {
     }
 }
 
-function wipeOutEffect(element, callback, customClass = 'fade-out') {
-    element.classList.add(customClass);
+function wipeOutEffect(element, callback) {
+    element.classList.add('fade-out');
     
     const transitionDuration = 800;
     setTimeout(() => {
-        element.classList.remove(customClass);
+        element.classList.remove('fade-out');
         callback();
     }, transitionDuration);
 }
 
+
 function showResult(finalPhrase, randomSaltMessage = null) {
     const circularWidget = document.querySelector('.circular-widget');
+    const resultContainer = document.createElement('div');
+    resultContainer.className = 'magic-result';
+    resultContainer.textContent = finalPhrase;
+    resultContainer.setAttribute('data-text', finalPhrase);
 
     wipeOutEffect(circularWidget, () => {
         circularWidget.innerHTML = '';
         const newStarsEffect = document.createElement('div');
         newStarsEffect.id = 'stars-effect';
         newStarsEffect.className = 'stars-effect';
+        starsEffect = newStarsEffect;
         circularWidget.appendChild(newStarsEffect);
-
-        const resultContainer = document.createElement('div');
-        resultContainer.className = 'magic-result';
-        resultContainer.textContent = finalPhrase;
-        resultContainer.setAttribute('data-text', finalPhrase);
-        resultContainer.style.opacity = "0";
         circularWidget.appendChild(resultContainer);
-
-        requestAnimationFrame(() => {
-            resultContainer.style.opacity = "1";
-        });
 
         if (randomSaltMessage !== null) {
             setTimeout(() => {
                 const saltLabel = document.createElement('div');
                 saltLabel.className = 'matrix-label';
                 saltLabel.textContent = "Your unique magic salt:";
-                saltLabel.style.opacity = "0";
                 circularWidget.appendChild(saltLabel);
 
                 const saltValue = document.createElement('code');
                 saltValue.className = 'matrix-code';
+
+                if (randomSaltMessage === magicSaltMessage) {
+                    saltValue.style.color = '#d43f4c';
+                }
+
                 saltValue.textContent = randomSaltMessage;
-                saltValue.style.opacity = "0";
                 circularWidget.appendChild(saltValue);
 
                 requestAnimationFrame(() => {
                     saltLabel.style.opacity = "1";
                     saltValue.style.opacity = "1";
                 });
-
             }, 1000);
         }
     });
@@ -139,11 +138,12 @@ async function getRandomNumberFree() {
         const result = await web3ForeTellerContract.methods.getRandomNumber().call();
         const randomSteps = Math.floor(Math.random() * 7);
         const finalPhrase = traverseList(Number(result), randomSteps);
-        showResult(finalPhrase, "To generate and use magic salt, burn some gas!");
+        showResult(finalPhrase, magicSaltMessage);
     } catch (error) {
         console.error("Error calling smart contract method:", error);
     }
 }
+
 
 
 async function getRandomNumberGasBurn() {
@@ -151,18 +151,11 @@ async function getRandomNumberGasBurn() {
         const web3ForeTellerContract = new web3.eth.Contract(contractABI, contractAddress);
         const accounts = await web3.eth.getAccounts();
         const fromAddress = accounts[0];
-        
-        // Burn some gas
         await web3ForeTellerContract.methods.getRandomSaltFromGasBurn().send({ from: fromAddress, gas: 200000 });
-
-        // Retrieve the random number
         const result = await web3ForeTellerContract.methods.getRandomNumberWithGas().call();
-
-        // Retrieve the randomSalt value
         const randomSaltValue = await web3ForeTellerContract.methods.randomSalt().call();
         const randomSteps = Math.floor(Math.random() * 7);
         const finalPhrase = traverseList(Number(result), randomSteps);
-
         showResult(finalPhrase, randomSaltValue); 
     } catch (error) {
         console.error("Error calling smart contract method:", error);
@@ -171,4 +164,4 @@ async function getRandomNumberGasBurn() {
 
 window.addEventListener("load", () => {
     connectToWeb3();
-});
+})
